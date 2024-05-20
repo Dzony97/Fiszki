674d46342315f885c5eec01d5e28432a0aadf9eb
Patrk Palonka <patrykpalonka12@gmail.com>
2024-05-20 16:42:34 +0200

    Problem Registration failed {'detail': 'Authentication credentials were not provided.'}

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, ListUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('users/', ListUserView.as_view(), name='users-list'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
+
 from .serializers import UserSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+class ListUserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
 
