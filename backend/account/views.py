7f9aa7e4e643b693e879c856f68046a824b2baa8
Patrk Palonka <patrykpalonka12@gmail.com>
2024-05-20 16:51:03 +0200

    Added permission_classes = [AllowAny] in RegisterView. Success registration working

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from .serializers import UserSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 
 class ListUserView(generics.ListAPIView):
